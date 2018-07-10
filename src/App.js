1ae55fc21c6c4c88d3004f84f7d45d86b31e9ab7
Vincent <vincent.lafargue66@gmail.com>
2018-07-10 20:15:50 +0200

    nettoyage pour le cours ocr

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          <form method="" action="">
-          <p><label>E-Mail <input type="mail" name="email" required autofocus value="voila email" /></label></p>
-          {/*le commentaire doit être entre {} et les expression jsx ne sont encore rien*/}
-          <p><button type="submit" value="Connexion" /></p>
-          </form>
-        </p>
-      </div>
-    );
+    return <div className="memory" />
   }
 }
 
